71d3ce6c4273cf05ec735a795dbd5d078d4961ff
limkim930 <1625753207lim@gmail.com>
2021-12-21 12:30:08 +0800

    about manager table

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import "element-ui/lib/theme-chalk/index.css";
 
 //导入组件
 // 导入自定义路由模块
-import APP from "../src/components/Sign.vue";
-import router from "./router_sign";
+// import APP from "../src/components/Sign.vue";
+// import router from "./router_sign";
 
 //剪切板组件
 // import VueClipboard from "vue-clipboard2";
@@ -18,8 +18,8 @@ import router from "./router_sign";
 // import APP from "../src/components/Company.vue";
 // import router from "./router_company";
 
-// import APP from "../src/components/Manager.vue"
-// import router from "./router_manager"
+import APP from "../src/components/Manager.vue";
+import router from "./router_manager";
 
 // 安装路由
 Vue.use(VueRouter);
