49a6dbd97adf2ee72937a1d99198d865b29b8338
limkim930 <1625753207lim@gmail.com>
2021-12-28 14:47:11 +0800

    new

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     if (location.search.split("?")[1] !== undefined) {
         if (location.search.split("?")[1].split("=")[0] === "type") {
             sessionStorage.setItem("jw_from", "hduhelp");
-            return window.location.href = "https://edu.limkim.cn/sign";
+            return window.location.href = "https://etdla.hduapp.com/sign";
         }
         const requestOptions = {
             method: "GET",
@@ -18,27 +18,27 @@
                 }));
                 if (sessionStorage.getItem("jw_from") && sessionStorage.getItem("jw_from") === "hduhelp") {
                     sessionStorage.removeItem("jw_from");
-                    window.location.href = "https://edu.limkim.cn/student/#/infoShare";
+                    window.location.href = "https://etdla.hduapp.com/student/#/infoShare";
                 }
                 else
-                    window.location.href = "https://edu.limkim.cn/student";
+                    window.location.href = "https://etdla.hduapp.com/student";
             }
             else if (JSON.parse(result).data.STAFFTYPE === "2") {
                 localStorage.setItem("jw_manager_file", JSON.stringify({
                     token: location.search.split("?")[1].split("&")[0].split("=")[1],
                     uname: location.search.split("?")[1].split("&")[1].split("=")[1]
                 }));
-                window.location.href = "https://edu.limkim.cn/manager";
+                window.location.href = "https://etdla.hduapp.com/manager";
             }
         }).catch(error => {
             alert(error);
         });
     }
     if (localStorage.getItem("jw_student_file") !== null)
-        return window.location.href = "https://edu.limkim.cn/student";
+        return window.location.href = "https://etdla.hduapp.com/student";
     if (localStorage.getItem("jw_ent_file") !== null)
-        return window.location.href = "https://edu.limkim.cn/company";
+        return window.location.href = "https://etdla.hduapp.com/company";
     if (localStorage.getItem("jw_manager_file") !== null)
-        return window.location.href = "https://edu.limkim.cn/manager";
-    window.location.href = "https://edu.limkim.cn/sign";
+        return window.location.href = "https://etdla.hduapp.com/manager";
+    window.location.href = "https://etdla.hduapp.com/sign";
 })();
