c9de9fba9c88cad0730bee996940e74b72c351c0
lim-kim930 <1625753207lim@gmail.com>
2022-09-08 14:01:29 +0800

    update api baseurl

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 //axios cookies配置
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "https://api.hduhelp.com/gormja_wrapper";
+axios.defaults.baseURL = "https://jl.hdu.edu.cn/gormja_wrapper";
 
 export default axios;
